efa88523ff289e897694e951ad273daf1a701dac
Ala Alduweebi <ala.alduweebi@outlook.de>
2026-03-19 20:22:03 +0100

    Refactor(Main): MainWindow in MainView umbenannt.

---
+++ ProMedScope/MainView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace ProMedScope
+{
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
