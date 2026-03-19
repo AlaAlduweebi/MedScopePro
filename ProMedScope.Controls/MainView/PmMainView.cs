00ff4b6ab502393c2789e8ef1227c5ccc96a24cf
Ala Alduweebi <ala.alduweebi@outlook.de>
2026-03-19 21:30:53 +0100

    Feat(MainView): MainView-Control, Template-Registrierung und Window-Style implementiert.

---
+++ ProMedScope.Controls/MainView/PmMainView.cs
@@ -0,0 +1,10 @@
+﻿using ProMedScope.Controls._Elements.ContentControl;
+
+namespace ProMedScope.Controls.MainView;
+
+/// <summary>
+/// Repräsentiert das Hauptfenster der Anwendung.
+/// </summary>
+public class PmMainView : PmContentControl
+{
+}
