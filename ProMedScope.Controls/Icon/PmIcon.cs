4d26e86a020bad21b06be29994eee52becde4dc5
Ala Alduweebi <ala.alduweebi@outlook.de>
2026-03-20 21:11:26 +0100

    Feat(Icon): Icon-Rendering über Geometry implementiert.

--- ProMedScope.Controls/Icon/PmIcon.cs
+++ ProMedScope.Controls/Icon/PmIcon.cs
@@ -1,4 +1,7 @@
 ﻿using ProMedScope.Controls._Elements.ContentControl;
+using ProMedScope.Controls.Icon;
+using System.Windows.Media;
+using System.Windows;
 
 namespace ProMedScope.Controls.Icon;
 
@@ -7,4 +10,105 @@ namespace ProMedScope.Controls.Icon;
 /// </summary>	
 public class PmIcon : PmContentControl
 {
+    #region Dependency Properties
+
+    public static readonly DependencyProperty IconMaterialProperty =
+        DependencyProperty.Register(nameof(IconMaterial), typeof(PmIconEnum), typeof(PmIcon), new PropertyMetadata(default(PmIconEnum), OnIconVisualChanged));
+
+    public static readonly DependencyProperty RenderedDrawingImageProperty =
+        DependencyProperty.Register(nameof(RenderedDrawingImage), typeof(ImageSource), typeof(PmIcon));
+
+    #endregion
+
+    #region Eigenschaften
+
+    /// <summary>
+    /// Repräsentiert das zentrale Symbol.
+    /// </summary>
+    public PmIconEnum IconMaterial
+    {
+        get { return (PmIconEnum)GetValue(IconMaterialProperty); }
+        set { SetValue(IconMaterialProperty, value); }
+    }
+
+    /// <summary>
+    /// Liefert das gerenderte Image des Icons.
+    /// </summary>
+    public ImageSource? RenderedDrawingImage
+    {
+        get => (ImageSource?)GetValue(RenderedDrawingImageProperty);
+        set => SetValue(RenderedDrawingImageProperty, value);
+    }
+
+    #endregion
+
+    #region Methoden
+
+    /// <summary>
+    /// Wird beim Laden des Control-Templates ausgeführt.
+    /// </summary>
+    public override void OnApplyTemplate()
+    {
+        base.OnApplyTemplate();
+        UpdateGeometryPaths();
+    }
+
+    /// <summary>
+    /// Wird aufgerufen, wenn sich eine Eigenschaft ändert und aktualisiert die Icon-Darstellung.
+    /// </summary>
+    private static void OnIconVisualChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is PmIcon icon)
+            icon.UpdateGeometryPaths();
+    }
+
+    /// <summary>
+    /// Aktualisiert das angezeigte Icon.
+    /// </summary>
+    private void UpdateGeometryPaths()
+    {
+        if (IconMaterial == PmIconEnum.None)
+        {
+            RenderedDrawingImage = null;
+            return;
+        }
+
+        var key = $"{IconMaterial}IconPath";
+
+        var geometry = TryFindResource(key) as Geometry;
+        if (geometry == null)
+        {
+            RenderedDrawingImage = null;
+            return;
+        }
+
+        RenderedDrawingImage = BuildDrawingImage(geometry);
+    }
+
+    /// <summary>
+    /// Erstellt ein DrawingImage basierend auf der angegebenen Geometrie.
+    /// </summary>
+    private static DrawingImage BuildDrawingImage(Geometry geometry)
+    {
+        return new DrawingImage(new DrawingGroup
+        {
+            Children =
+        {
+            BuildGeometryDrawing(geometry)
+        }
+        });
+    }
+
+    /// <summary>
+    /// Erstellt eine GeometryDrawing.
+    /// </summary>
+    private static GeometryDrawing BuildGeometryDrawing(Geometry geometry)
+    {
+        return new GeometryDrawing
+        {
+            Geometry = geometry
+        };
+    }
+
+    #endregion
 }
