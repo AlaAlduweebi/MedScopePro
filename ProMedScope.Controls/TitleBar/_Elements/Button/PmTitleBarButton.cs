6d0de7b3dbea71a627f5df2f7c6349c2047a0773
Ala Alduweebi <ala.alduweebi@outlook.de>
2026-03-20 11:46:36 +0100

    Feat(TitleBar): Profil-Button hinzugefügt.

---
+++ ProMedScope.Controls/TitleBar/_Elements/Button/PmTitleBarButton.cs
@@ -0,0 +1,30 @@
+﻿using ProMedScope.Controls._Elements.Button.Generic;
+using System.Windows;
+
+namespace ProMedScope.Controls.TitleBar._Elements.Button;
+
+/// <summary>
+/// /// Benutzerdefinierter Button für die TitleBar.
+/// </summary>
+public class PmTitleBarButton : PmButtonGeneric<PmTitleBarButtonEnum>
+{
+    #region Methoden
+
+    /// <summary>
+    /// Event-Handler für Button-Klick.
+    /// Führt die entsprechende Aktion basierend auf dem <see cref="ButtonName"/> aus.
+    /// </summary>
+    protected override void ButtonClick(object sender, RoutedEventArgs e)
+    {
+        switch (ButtonName)
+        {
+            case PmTitleBarButtonEnum.Profile:
+                break;
+
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    #endregion
+}
---
+++ ProMedScope.Controls/TitleBar/_Elements/Button/PmTitleBarButtonEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace ProMedScope.Controls.TitleBar._Elements;
+
+/// <summary>
+/// Definiert die Button-Typen, die in der TitleBar verwendet werden.
+/// </summary>
+public enum PmTitleBarButtonEnum
+{
+    Profile,
+}
