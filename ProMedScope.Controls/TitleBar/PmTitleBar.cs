cc7a0922682db68cba3b8ceff23ec95d61f1656a
Ala Alduweebi <ala.alduweebi@outlook.de>
2026-03-20 06:51:54 +0100

    Add(TitleBar): Control und Template hinzugefügt.

---
+++ ProMedScope.Controls/TitleBar/PmTitleBar.cs
@@ -0,0 +1,10 @@
+﻿using ProMedScope.Controls._Elements.ContentControl;
+
+namespace ProMedScope.Controls.TitleBar;
+
+/// <summary>
+/// Benutzerdefinierte TitleBar.
+/// </summary>
+public class PmTitleBar : PmContentControl
+{
+}
