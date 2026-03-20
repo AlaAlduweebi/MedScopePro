2575c8f7d8c2649c4553bac6a9dc33779da6b408
Ala Alduweebi <ala.alduweebi@outlook.de>
2026-03-20 20:17:12 +0100

    Feat(Icon): Control hinzugefügt.

---
+++ ProMedScope.Controls/Icon/PmIcon.cs
@@ -0,0 +1,10 @@
+﻿using ProMedScope.Controls._Elements.ContentControl;
+
+namespace ProMedScope.Controls.TitleBar;
+
+/// <summary>
+/// Benutzerdefiniertes Icon.
+/// </summary>	
+public class PmIcon : PmContentControl
+{
+}
