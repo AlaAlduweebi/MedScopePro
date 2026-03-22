579b31ac9352745530969753f5dfa019af621815
Ala Alduweebi <ala.alduweebi@outlook.de>
2026-03-22 16:17:32 +0100

    Add(Views): UserControls für die TabItems hinzugefügt.

---
+++ ProMedScope.Views/Recordings/RecordingsView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProMedScope.Views.Recordings
+{
+    /// <summary>
+    /// Interaktionslogik für RecordingsView.xaml
+    /// </summary>
+    public partial class RecordingsView : UserControl
+    {
+        public RecordingsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
