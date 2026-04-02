5b19cbf86b8faeb694ac8dd72510b88701309296
Ala Alduweebi <ala.alduweebi@outlook.de>
2026-04-02 17:44:01 +0200

    Style(Workspace): TabControl + TabItem gestylt & registriert.

--- ProMedScope.Controls/Workspace/_Elements/Item/PmWorkspaceItem.cs
+++ ProMedScope.Controls/Workspace/_Elements/Item/PmWorkspaceItem.cs
@@ -1,10 +1,10 @@
 ﻿using ProMedScope.Controls._Elements.TabControl.TabItem;
 
-namespace ProMedScope.Controls.PmWorkspace._Elements.Item;
+namespace ProMedScope.Controls.Workspace._Elements.Item;
 
 /// <summary>
 /// Repräsentiert ein einzelnes Workspace-Tab-Item.
 /// </summary>
-public class PmPmWorkspaceItem : PmTabControlTabItem
+public class PmWorkspaceItem : PmTabControlTabItem
 {
 }
