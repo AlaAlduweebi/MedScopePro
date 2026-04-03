5e0edc742705e3765df449963400c8eb6fe8aba4
Ala Alduweebi <ala.alduweebi@outlook.de>
2026-04-03 20:32:36 +0200

    Fix(Workspace): benutzerdefinierten TabItem-Container für Workspace aktiviert.

--- ProMedScope.Controls/Workspace/PmWorkspace.cs
+++ ProMedScope.Controls/Workspace/PmWorkspace.cs
@@ -1,4 +1,6 @@
-﻿using ProMedScope.Controls._Elements.TabControl;
+﻿using ProMedScope.Controls.Workspace._Elements.Item;
+using ProMedScope.Controls._Elements.TabControl;
+using System.Windows;
 
 namespace ProMedScope.Controls.Workspace;
 
@@ -7,4 +9,11 @@ namespace ProMedScope.Controls.Workspace;
 /// </summary>
 public class PmWorkspace : PmTabControl
 {
+    /// <summary>
+    /// Erstellt einen neuen Container für ein Workspace-Element.
+    /// </summary>
+    protected override DependencyObject GetContainerForItemOverride()
+    {
+        return new PmWorkspaceItem();
+    }
 }
