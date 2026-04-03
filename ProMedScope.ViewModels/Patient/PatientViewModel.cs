61c39ca8ed6e9dec29bd8cf92759d0a22b05748c
Ala Alduweebi <ala.alduweebi@outlook.de>
2026-04-03 11:42:57 +0200

    Feat(Patient): Beispiel-Patienten an Workspace gebunden.

--- ProMedScope.ViewModels/Patient/PatientViewModel.cs
+++ ProMedScope.ViewModels/Patient/PatientViewModel.cs
@@ -1,5 +1,21 @@
-﻿namespace ProMedScope.ViewModels.Patient;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
 
-public class PatientViewModel
-{ 
+namespace ProMedScope.ViewModels.Patient;
+
+/// <summary>
+/// Repräsentiert das ViewModel der Patientenansicht.
+/// </summary>
+public class PatientViewModel : ObservableObject
+{
+    /// <summary>
+    /// Enthält die geöffneten Patienten im Workspace.
+    /// </summary>
+    public ObservableCollection<string> OpenPatients { get; } =
+    [
+        "Virginia Rossi",
+        "Marco Bianchi",
+        "Giulia Romano",
+        "Sofia Esposito"
+    ];
 }
