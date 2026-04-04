ae4f46963a7b3e65940cf43bb50ea69671be37c2
Ala Alduweebi <ala.alduweebi@outlook.de>
2026-04-04 10:32:35 +0200

    Add(Workspace): Close-Button für das WorkspaceItem hinzugefügt.

---
+++ ProMedScope.Controls/Workspace/_Elements/Item/_Elements/Button/PmWorkspaceItemButton.cs
@@ -0,0 +1,38 @@
+﻿using ProMedScope.Controls._Elements.Button.Generic;
+using System.Windows;
+
+namespace ProMedScope.Controls.Workspace._Elements.Item._Elements.Button;
+
+/// <summary>
+/// /// Benutzerdefinierter Button für das WorkspaceItem.
+/// </summary>
+public class PmWorkspaceItemButton : PmButtonGeneric<PmWorkspaceItemButtonEnum>
+{
+    #region Methoden
+
+    /// <summary>
+    /// Event-Handler für Button-Klick.
+    /// Führt die entsprechende Aktion basierend auf dem <see cref="ButtonName"/> aus.
+    /// </summary>
+    protected override void ButtonClick(object sender, RoutedEventArgs e)
+    {
+        switch (ButtonName)
+        {
+            case PmWorkspaceItemButtonEnum.Close:
+                CloseWorkspaceItem();
+                break;
+
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    /// <summary>
+    /// Schließt das WorkspaceItem.
+    /// </summary>
+    public void CloseWorkspaceItem()
+    {
+    }
+
+    #endregion
+}
---
+++ ProMedScope.Controls/Workspace/_Elements/Item/_Elements/Button/PmWorkspaceItemButtonEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace ProMedScope.Controls.Workspace._Elements.Item._Elements.Button;
+
+/// <summary>
+/// Definiert die Button-Typen, die in dem WorkspaceItem verwendet werden.
+/// </summary>
+public enum PmWorkspaceItemButtonEnum
+{
+    Close
+}
